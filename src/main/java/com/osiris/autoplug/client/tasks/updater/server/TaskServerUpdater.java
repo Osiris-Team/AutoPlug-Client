aef55030a616cb667933f9cae6d462d3f45037e3
Kuma <20806030+lkkuma@users.noreply.github.com>
2022-03-13 03:10:12 -0400

    Initial FabricMC implementation
    #143
    Initial implementation for FabricMC automation

--- src/main/java/com/osiris/autoplug/client/tasks/updater/server/TaskServerUpdater.java
+++ src/main/java/com/osiris/autoplug/client/tasks/updater/server/TaskServerUpdater.java
@@ -60,6 +60,8 @@ public class TaskServerUpdater extends BetterThread {
 
         if (updaterConfig.server_github_repo_name.asString() != null || updaterConfig.server_jenkins_project_url.asString() != null) {
             doAlternativeUpdatingLogic();
+        } else if (serverSoftware.equalsIgnoreCase("fabric")) {
+            doFabricUpdatingLogic();
         } else if (serverSoftware.equalsIgnoreCase("purpur")) {
             doPurpurUpdatingLogic();
         } else {
@@ -251,6 +253,143 @@ public class TaskServerUpdater extends BetterThread {
                                 setSuccess(false);
                             }
                         }
+
+                    } else {
+                        setStatus("Server update failed!");
+                        setSuccess(false);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    private void doFabricUpdatingLogic() throws WrongJsonTypeException, IOException, HttpErrorException, InterruptedException, YamlWriterException, DuplicateKeyException, YamlReaderException, IllegalListException, NoSuchAlgorithmException {
+        FabricDownloadsAPI fabricDownloadsAPI = new FabricDownloadsAPI();
+        String[] buildId = updaterConfig.server_build_id.getValue().asArraySplitByColons();
+        String[] loaderId = buildId[0].split(".");
+        int loaderMajorId = Integer.valueOf(loaderId[0]);
+        int loaderMinorId = Integer.valueOf(loaderId[1]);
+        int loaderBuildId = Integer.valueOf(loaderId[2]);
+        String[] installerId = buildId[1].split(".");
+        int installerMajorId = Integer.valueOf(installerId[0]);
+        int installerMinorId = Integer.valueOf(installerId[1]);
+        int installerBuildId = Integer.valueOf(installerId[2]);
+        JsonObject latestLoader = fabricDownloadsAPI.getLatestLoader();
+        int latestLoaderMajorId = latestLoader.get("majorID").getAsInt();
+        int latestLoaderMinorId = latestLoader.get("minorID").getAsInt();
+        int latestLoaderBuildId = latestLoader.get("buildID").getAsInt();
+        JsonObject latestInstaller = fabricDownloadsAPI.getLatestInstaller(serverVersion);
+        int latestInstallerMajorId = latestInstaller.get("majorID").getAsInt();
+        int latestInstallerMinorId = latestInstaller.get("minorID").getAsInt();
+        int latestInstallerBuildId = latestInstaller.get("buildID").getAsInt();
+        String[] latestBuildId = {latestLoader.get("version").getAsString(), latestInstaller.get("version").getAsString()};
+        //JsonObject latestBuild = fabricDownloadsAPI.getLatestLoader();
+        //int latestBuildId = latestBuild.get("build").getAsInt();
+        //String buildHash = latestBuild.get("md5").getAsString();
+        String url = fabricDownloadsAPI.getLatestDownloadUrl(serverVersion, latestLoader.get("version").getAsString(), latestInstaller.get("version").getAsString());
+
+        // Check if the latest build-id is bigger than our current one.
+        if (
+            latestLoaderMajorId <= loaderMajorId &&
+            latestLoaderMinorId <= loaderMinorId &&
+            latestLoaderBuildId <= loaderBuildId &&
+            latestInstallerMajorId <= installerMajorId &&
+            latestInstallerMinorId <= installerMinorId &&
+            latestInstallerBuildId <= installerBuildId
+        ) {
+            setStatus("Your server is on the latest version!");
+            setSuccess(true);
+            return;
+        }
+
+        if (profile.equals("NOTIFY")) {
+            setStatus("Update found (" + buildId + " -> " + latestBuildId + ")!");
+        } else if (profile.equals("MANUAL")) {
+            setStatus("Update found (" + buildId + " -> " + latestBuildId + "), started download!");
+
+            // Download the file
+            File cache_dest = new File(downloadsDir.getAbsolutePath() + "/" + serverSoftware + "-latest.jar");
+            if (cache_dest.exists()) cache_dest.delete();
+            cache_dest.createNewFile();
+            TaskDownload download = new TaskDownload("ServerDownloader", getManager(), url, cache_dest, true);
+            download.start();
+
+            while (true) {
+                Thread.sleep(500); // Wait until download is finished
+                if (download.isFinished()) {
+                    if (download.isSuccess()) {
+                        setStatus("Server update downloaded.");
+                        /*setStatus("Server update downloaded. Checking hash...");
+                        if (download.compareWithMD5(buildHash)) {
+                            setStatus("Server update downloaded successfully.");
+                            setSuccess(true);
+                        } else {
+                            setStatus("Downloaded server update is broken (hash check failed). Nothing changed!");
+                            setSuccess(false);
+                        } */
+
+                    } else {
+                        setStatus("Server update failed!");
+                        setSuccess(false);
+                    }
+                    break;
+                }
+            }
+        } else {
+            setStatus("Update found (" + buildId + " -> " + latestBuildId + "), started download!");
+
+            // Download the file
+            File cache_dest = new File(downloadsDir.getAbsolutePath() + "/" + serverSoftware + "-latest.jar");
+            if (cache_dest.exists()) cache_dest.delete();
+            cache_dest.createNewFile();
+            TaskDownload download = new TaskDownload("ServerDownloader", getManager(), url, cache_dest, true);
+            download.start();
+
+            while (true) {
+                Thread.sleep(500);
+                if (download.isFinished()) {
+                    if (download.isSuccess()) {
+                        setStatus("Server update downloaded.");
+                        /*if (download.compareWithMD5(buildHash)) {
+                            File final_dest = GD.SERVER_JAR;
+                            if (final_dest == null)
+                                final_dest = new File(GD.WORKING_DIR + "/" + serverSoftware + "-latest.jar");
+                            if (final_dest.exists()) final_dest.delete();
+                            final_dest.createNewFile();
+                            FileUtils.copyFile(cache_dest, final_dest);
+                            setStatus("Server update was installed successfully (" + buildId + " -> " + latestBuildId + ")!");
+                            updaterConfig.server_build_id.setValues("" + latestBuildId);
+                            updaterConfig.save();
+                            setSuccess(true);
+                        } else {
+                            if (updaterConfig.server_skip_hash_check.asBoolean()) {
+                                addWarning("Note that the hash check failed for this download, but installing anyways because skip-hash-check is enabled!");
+                                File final_dest = GD.SERVER_JAR;
+                                if (final_dest == null)
+                                    final_dest = new File(GD.WORKING_DIR + "/" + serverSoftware + "-latest.jar");
+                                if (final_dest.exists()) final_dest.delete();
+                                final_dest.createNewFile();
+                                FileUtils.copyFile(cache_dest, final_dest);
+                                setStatus("Server update was installed successfully (" + buildId + " -> " + latestBuildId + ")!");
+                                updaterConfig.server_build_id.setValues("" + latestBuildId);
+                                updaterConfig.save();
+                                setSuccess(true);
+                            } else {
+                                setStatus("Downloaded server update is broken (hash check failed). Nothing changed!");
+                                setSuccess(false);
+                            }
+                        }*/
+                        File final_dest = GD.SERVER_JAR;
+                        if (final_dest == null)
+                            final_dest = new File(GD.WORKING_DIR + "/" + serverSoftware + "-latest.jar");
+                        if (final_dest.exists()) final_dest.delete();
+                        final_dest.createNewFile();
+                        FileUtils.copyFile(cache_dest, final_dest);
+                        setStatus("Server update was installed successfully (" + buildId + " -> " + latestBuildId + ")!");
+                        updaterConfig.server_build_id.setValues("" + String.join(",", latestBuildId));
+                        updaterConfig.save();
+                        setSuccess(true);
 
                     } else {
                         setStatus("Server update failed!");
