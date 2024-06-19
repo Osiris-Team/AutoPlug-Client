a78b002d5f1608d91f06f4b8df3cb6d483b04ff7
vengestone-dragoon <97992930+vengestone-dragoon@users.noreply.github.com>
2024-06-19 12:21:49 -0400

    Update CustomCheckURL.java

--- src/main/java/com/osiris/autoplug/client/tasks/updater/search/CustomCheckURL.java
+++ src/main/java/com/osiris/autoplug/client/tasks/updater/search/CustomCheckURL.java
@@ -1,11 +1,3 @@
-/*
- * Copyright (c) 2022-2023 Osiris-Team.
- * All rights reserved.
- *
- * This software is copyrighted work, licensed under the terms
- * of the MIT-License. Consult the "LICENSE" file for details.
- */
-
 package com.osiris.autoplug.client.tasks.updater.mods;
 
 import com.google.gson.JsonObject;
