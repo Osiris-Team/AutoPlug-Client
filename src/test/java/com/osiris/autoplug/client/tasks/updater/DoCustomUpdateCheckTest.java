e90b323c2d1174e320066674e7f9a10e3ffe3750
vengestone-dragoon <97992930+vengestone-dragoon@users.noreply.github.com>
2024-06-20 13:17:57 -0400

    Update DoCustomUpdateCheckTest.java

--- src/test/java/com/osiris/autoplug/client/tasks/updater/DoCustomUpdateCheckTest.java
+++ src/test/java/com/osiris/autoplug/client/tasks/updater/DoCustomUpdateCheckTest.java
@@ -1,3 +1,6 @@
-
 package com.osiris.autoplug.client.tasks.updater;
+
+import com.osiris.autoplug.client.tasks.updater.plugins.MinecraftPlugin;
+import com.osiris.autoplug.client.tasks.updater.search.SearchResult;
+
 
