d87a0f901eab80c6ed4fafacb03fd559291b928b
vengestone-dragoon <97992930+vengestone-dragoon@users.noreply.github.com>
2024-06-22 17:52:36 -0400

    Update DoCustomUpdateCheckTest.java

--- src/test/java/com/osiris/autoplug/client/tasks/updater/DoCustomUpdateCheckTest.java
+++ src/test/java/com/osiris/autoplug/client/tasks/updater/DoCustomUpdateCheckTest.java
@@ -3,7 +3,10 @@ package com.osiris.autoplug.client.tasks.updater;
 import com.osiris.autoplug.client.tasks.updater.plugins.MinecraftPlugin;
 import com.osiris.autoplug.client.tasks.updater.search.SearchResult;
 import com.osiris.autoplug.client.tasks.updater.search.CustomCheckURL;
+import com.osiris.autoplug.client.tasks.updater.mods.ModrinthAPI;
 import com.osiris.autoplug.client.UtilsTest;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class TestCustomUpdateCheck {
@@ -12,6 +15,6 @@ class TestCustomUpdateCheck {
       MinecraftPlugin pl = new MinecraftPlugin("./plugins/", "Chunky", "0.0.0", "pop4959", 0, 0, null);
       pl.customCheckURL = "https://api.modrinth.com/v2/project/chunky/version";
       SearchResult sr = new CustomCheckURL().doCustomCheck(pl);
-      assertTrue(expected, actual);
+      assertTrue(1 == sr.resultCode);
     }
 }
