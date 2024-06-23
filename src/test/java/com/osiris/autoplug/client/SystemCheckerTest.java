54fd737ee97b5161ae469f39fbaa6bc4c9fc476d
vengestone-dragoon <97992930+vengestone-dragoon@users.noreply.github.com>
2024-06-22 20:00:39 -0400

    return SystemCheckerTest to original
    uncomment lines that werer throwing errors for me

--- src/test/java/com/osiris/autoplug/client/SystemCheckerTest.java
+++ src/test/java/com/osiris/autoplug/client/SystemCheckerTest.java
@@ -15,13 +15,13 @@ import java.net.InetAddress;
 import java.net.URL;
 
 class SystemCheckerTest {
-/*
+
     @Test
     void checkInternetAccessMethod1() throws Exception {
         boolean reachable = InetAddress.getByName("www.google.com").isReachable(10000);
         if (!reachable) throw new Exception("Failed to reach www.google.com!");
     }
-*/
+
     @Test
     void checkInternetAccessMethod2() throws Exception {
         HttpURLConnection connection = (HttpURLConnection) new URL("https://www.google.com").openConnection();
