f8879622ef6cf61ad6ce8aa12ae5de593bced7a7
Zoe <zoe+1@zmp.lol>
2024-06-24 10:07:55 -0700

    Replaced the Copyright messages (were missing)

--- src/main/java/com/osiris/autoplug/client/network/online/connections/SSHServerConsoleReceive.java
+++ src/main/java/com/osiris/autoplug/client/network/online/connections/SSHServerConsoleReceive.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2021-2023 Osiris-Team.
+ * All rights reserved.
+ *
+ * This software is copyrighted work, licensed under the terms
+ * of the MIT-License. Consult the "LICENSE" file for details.
+ */
 package com.osiris.autoplug.client.network.online.connections;
 
 import com.osiris.autoplug.client.console.Commands;
